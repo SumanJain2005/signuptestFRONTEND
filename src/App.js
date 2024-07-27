dd035ef220514ca5c65dba30cd1fffa5a556d402
Suman Naresh Jain <138118525+SumanJain2005@users.noreply.github.com>
2024-07-28 01:22:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './Signup';
 import Login from './Login';
 
